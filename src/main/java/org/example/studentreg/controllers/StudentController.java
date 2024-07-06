a4fa3557aafe0defe97221d0cd98202d8e1a35cc
dreaca <buddhikadreaca@gmail.com>
2024-07-06 16:24:01 +0530

    doGet method

--- src/main/java/org/example/studentreg/controllers/StudentController.java
+++ src/main/java/org/example/studentreg/controllers/StudentController.java
@@ -16,15 +16,57 @@ import org.example.studentreg.dto.StudentDTO;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @WebServlet(urlPatterns = "/student")
 public class StudentController extends HttpServlet {
+    Connection connection;
+    static String SAVE_STUDENT = "INSERT INTO student(id,name,city,level,email) VALUES (?,?,?,?,?)";
+    static String GET_STUDENT = "SELECT * FROM student WHERE id = ?";
+
+    @Override
+    public void init() {
+        try {
+            var driVerClass = getServletContext().getInitParameter("driver");
+            var dbURL = getServletContext().getInitParameter("dbURL");
+            var user = getServletContext().getInitParameter("dbUsername");
+            var password = getServletContext().getInitParameter("dbPassword");
+            Class.forName(driVerClass);
+            this.connection = DriverManager.getConnection(dbURL, user, password);
+
+        }catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Todo : get student details
+        //Todo : get student
+        var studentDTO = new StudentDTO();
+        try(var writer = resp.getWriter()){
+            PreparedStatement pstm = connection.prepareStatement(GET_STUDENT);
+            String id = req.getParameter("id");
+            pstm.setString(1,id);
+            ResultSet resultSet = pstm.executeQuery();
+            while (resultSet.next()){
+
+                studentDTO.setId(resultSet.getString("id"));
+                studentDTO.setName(resultSet.getString("name"));
+                studentDTO.setCity(resultSet.getString("city"));
+                studentDTO.setLevel(resultSet.getString("level"));
+                studentDTO.setEmail(resultSet.getString("email"));
+            }
+            resp.setContentType("application/json");
+            var json = JsonbBuilder.create();
+            json.toJson(studentDTO, resp.getWriter());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
@@ -54,7 +96,6 @@ public class StudentController extends HttpServlet {
         JsonObject jsonObject = reader.readObject();
         System.out.println(jsonObject.getString("name"));*/
 
-
         /*JsonReader reader2 = Json.createReader(req.getReader());
         JsonArray array = reader2.readArray();
         for (int i = 0; i < array.size(); i++) {
@@ -62,16 +103,32 @@ public class StudentController extends HttpServlet {
         }*/
         // JSON array with bind function
 
-        Jsonb jsonb = JsonbBuilder.create();
+        /*Jsonb jsonb = JsonbBuilder.create();
         List<StudentDTO> students = jsonb.fromJson(req.getReader(),new ArrayList<StudentDTO>(){}.getClass().getGenericSuperclass());
-        students.forEach(System.out::println);
+        students.forEach(System.out::println);*/
 
+        Jsonb jsonb = JsonbBuilder.create();
+        StudentDTO sdto = jsonb.fromJson(req.getReader(), StudentDTO.class);
+        try{
+            var preparedStatement = connection.prepareStatement(SAVE_STUDENT);
+
+            preparedStatement.setString(1, sdto.getId());
+            preparedStatement.setString(2, sdto.getName());
+            preparedStatement.setString(3,sdto.getCity());
+            preparedStatement.setString(4,sdto.getLevel());
+            preparedStatement.setString(5, sdto.getEmail());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //Todo : delete student
+
     }
 
     @Override
